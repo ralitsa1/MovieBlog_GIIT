8306ec5346283145827320a71da30d0579a92f0f
DianaMcO <dianamcode@outlook.com>
2017-05-15 20:34:58 +0100

    Movies, DataObject and config all work together
    The config file just sets up the constants for the application. So far, only the info for the DB connections exists.
    
    The DataObject file would be the parent file for all the classes. I think that most of our classes would be used to read data and enter data. This is just an idea but the database $pdo object is written just once and created every time we instantiate an object from a class.
    
    The Movies class defines the genres and reads a genre from a movies table...not sure it will work as I have not tested.
    
    Once we have discussed how we are going to structure our data, we could probably create a new repo and pull the required info in.

---
+++ DataObject.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * Description of DataObject
+ * 
+ * This class is the parent class for all the classes. It is an abstract class used to create new classes 
+ * which will handle database access and retrieval
+ *
+ * @author diana
+ */
+
+require_once "config.php";                       //the config.php file contains the constants used withing the classes
+
+abstract class DataObject {
+    
+    protected $data = [];                       //creates an empty array that will hold the data from the record in the DB
+
+    public function __construct($data) {            //constructor accepts data from the DB in the form of an associative array
+        foreach ($data as $key => $value) {         //with field names and values
+            if (array_key_exists($key, $this->data)) //$data array then stores the field name and values
+                $this->data[$key] = $value;
+        }
+    }
+
+    public function getValue($field) {              // Accepts field name and looks up the name in the object $data array   
+        if (array_key_exists($field, $this->data)) {//it enables outside code to access the data in the object
+            return $this->data[$field];
+        } else {
+            die ("Field not found");
+        }
+    }
+
+   
+    protected function connect() {                  //Connect to the database
+        try {
+            $conn = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD);
+            $conn->setAttribute(PDO::ATTR_PERSISTENT, true);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } 
+        catch (PDOException $e) {
+            die("Connection failed: “ . $e->getMessage() ");
+        }
+        return $conn;
+    }
+
+    protected function disconnect($conn) {          //Disconnect the database
+        $conn = "";
+    }
+
+}
+?>
+
---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+  A configuration file that lists all the constants used to configure the application
+ */
+
+const DB_DSN = 'mysql:host=localhost;dbname=SHOPPING_CART'; //Database name
+const DB_USER = 'root';                             // Database username
+const DB_PASS = '';                                 //Database password
+?>
+
